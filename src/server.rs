76287ce601f9d93d0342e5f3e94b5a4bd95f7bd5
Diversable <dan.mantei@outlook.com>
2024-02-05 14:47:11 -0800

    fix: finding *.js & *.wasm files

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,20 @@
-use leptos_spin::{render_best_match_to_stream_with_context, RouteTable, server_fn::register_explicit};
+use leptos::provide_context;
+use leptos_spin::{
+    render_best_match_to_stream_with_context, server_fn::register_explicit, RouteTable,
+};
 use spin_sdk::http::{IncomingRequest, ResponseOutparam};
 use spin_sdk::{http_component, sqlite::Connection};
-use leptos::provide_context;
 use std::sync::Arc;
 
 #[http_component]
 async fn handle_spin_test(req: IncomingRequest, resp_out: ResponseOutparam) {
     let mut conf = leptos::get_configuration(None).await.unwrap();
-    conf.leptos_options.output_name = "spin_test".to_owned();
+    conf.leptos_options.output_name = "benwis-leptos".to_owned();
 
     // Register server functions
 
-    register_explicit::<crate::pages::home::GetCount>();
-    register_explicit::<crate::pages::home::UpdateCount>();
+    // register_explicit::<crate::pages::home::GetCount>();
+    // register_explicit::<crate::pages::home::UpdateCount>();
 
     let app_router = crate::routes::AppRouter;
 
@@ -21,5 +23,15 @@ async fn handle_spin_test(req: IncomingRequest, resp_out: ResponseOutparam) {
 
     let con = Arc::new(Connection::open("default").expect("Failed to open benwis_leptos db"));
 
-    render_best_match_to_stream_with_context(req, resp_out, &routes, app_router,move ||{provide_context(con.clone()); }, &conf.leptos_options).await
+    render_best_match_to_stream_with_context(
+        req,
+        resp_out,
+        &routes,
+        app_router,
+        move || {
+            provide_context(con.clone());
+        },
+        &conf.leptos_options,
+    )
+    .await
 }
