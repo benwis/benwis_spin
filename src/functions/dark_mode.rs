31de224a9f1b7b83b4e126aa8b004f19c38cee5b
benwis <ben@celcyon.com>
2024-03-07 08:04:02 -0800

    Fixed default redirect, removing custom redirect for dark mode

--- src/functions/dark_mode.rs
+++ src/functions/dark_mode.rs
@@ -8,23 +8,9 @@ pub async fn toggle_dark_mode(prefers_dark: bool) -> Result<bool, ServerFnError>
     let req = expect_context::<RequestParts>();
     use std::borrow::Cow;
 
-    //let referrer:String = req.headers().iter().find(|(k,v)| k=="Referer").map(|(k, v)| v).ok_or_else(ServerFnError::Request("No Referer header provided!".to_string()))?;
-
-    let referrer: Vec<(&String, String)> = req
-        .headers()
-        .iter()
-        .filter(|(k, _v)| k == "referer")
-        .map(|(k, v)| (k, String::from_utf8_lossy(v).to_string()))
-        .collect();
-    let referrer = referrer.first().map(|(k, v)| v);
-    let Some(referrer) = referrer else {
-        return Err(ServerFnError::new("No Referer Provided".to_string()))
-    };
-    println!("REFERRER: {referrer}");
     let res_options =
         use_context::<ResponseOptions>().expect("to have leptos_spin::ResponseOptions provided");
     let cookie = format!("darkmode={};Path=/",prefers_dark );
     res_options.insert_header("Set-Cookie", cookie.as_bytes());
-    redirect(referrer);
     Ok(prefers_dark)
 }
