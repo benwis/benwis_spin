0274f4272ea620eb6538758d82e059930e25d106
John Funk <john@underscorefunk.com>
2024-04-16 17:49:42 -0500

    Fix portfolio post cards

--- src/components/portfolio_card.rs
+++ src/components/portfolio_card.rs
@@ -10,10 +10,10 @@ pub fn PortfolioCard(
 ) -> impl IntoView {
     view! {
         <a class="post-card" href=href>
-            <img class="post-card__underlay" src=img alt=""/>
-            <div class="post-card__layout">
-                <h3 class="post-card__heading">{heading}</h3>
-                <p class="post-card__meta">{description}</p>
+            <img class="post-card__image" src=img alt=""/>
+            <div class="post-card__text">
+                <h2 class="post-card__heading">{heading}</h3>
+                <p class="post-card__excerpt">{description}</p>
             </div>
         </a>
     }
