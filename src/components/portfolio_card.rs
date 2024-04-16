5b85027b531eb7d5ece5b23a9569a70acdca1634
John Funk <john@underscorefunk.com>
2024-04-16 17:34:18 -0500

    Simplify portfolio items

--- src/components/portfolio_card.rs
+++ src/components/portfolio_card.rs
@@ -9,23 +9,12 @@ pub fn PortfolioCard(
     description: String,
 ) -> impl IntoView {
     view! {
-        <section>
-            <a href=href>
-                <div class="space-y-4">
-                    <div class="aspect-w-3 aspect-h-2">
-                        <img class="object-cover h-200px shadow-lg rounded-lg" src=img alt=""/>
-                    </div>
-                    <div class="space-y-2">
-                        <div class="text-lg leading-6 font-medium space-y-1">
-                            <h3 class="dark:text-white text-black">{heading}</h3>
-                            <p class="text-indigo-600">{sub_heading}</p>
-                        </div>
-                        <div role="list" class="flex space-x-5 no-underline">
-                            <p class="dark:text-white text-black">{description}</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </section>
+        <a class="post-card" href=href>
+            <img class="post-card__underlay" src=img alt=""/>
+            <div class="post-card__layout">
+                <h3 class="post-card__heading">{heading}</h3>
+                <p class="post-card__meta">{description}</p>
+            </div>
+        </a>
     }
 }
